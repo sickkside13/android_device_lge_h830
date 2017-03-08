189fe5c8cacca0790d0ad6045dd04e8d03f6411b
sickkside13 <sickksidex3@gmail.com>
2017-03-08 13:00:50 -0600

    Update and rename lineage.mk to du.mk

--- du.mk
+++ du.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/h830/full_h830.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
-# Overlays (inherit after vendor/cm to ensure we override it)
+# Overlays (inherit after vendor/du to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := lineage_h830
+PRODUCT_NAME := du_h830
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g5" \
